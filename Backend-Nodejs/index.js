5132b6a3c660a7d1e600409ad388e8e38bb3649d
MatiEvaDan <matilde.danielsen@gmail.com>
2026-04-21 15:57:53 +0200

    fix port and make inventory screen

--- Backend-Nodejs/index.js
+++ Backend-Nodejs/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const app = require('./app');
 //lille ændringer
 
-const PORT = process.env.Port || 3000;
+const PORT = process.env.PORT || 3000;
 
 const MONGO_URI = process.env.MONGO_URI;
 
