fd7e34bfb3fd4609fc095b0fc379cceeb64ca267
MatiEvaDan <matilde.danielsen@gmail.com>
2026-04-21 16:13:24 +0200

    test besked

--- Backend-Nodejs/index.js
+++ Backend-Nodejs/index.js
@@ -9,6 +9,8 @@ const MONGO_URI = process.env.MONGO_URI;
 mongoose.connect(MONGO_URI)
 .then(() => {
     console.log("Forbundet til MongoDB");
+    
+    console.log("🚀 NY VERSION DEPLOYET");
 
     app.listen(PORT,'0.0.0.0', () => {
         console.log(`Server kører på port ${PORT}`);
