13eaa6c3416971538a378cff5d41dcb795b7d3f7
MatiEvaDan <matilde.danielsen@gmail.com>
2026-04-22 09:48:10 +0200

    fix

--- Backend-Nodejs/index.js
+++ Backend-Nodejs/index.js
@@ -6,15 +6,15 @@ const PORT = process.env.PORT || 3000;
 
 const MONGO_URI = process.env.MONGO_URI;
 
+app.listen(PORT,'0.0.0.0', () => {
+        console.log(`Server kører på port ${PORT}`);
+    });
+
 mongoose.connect(MONGO_URI)
 .then(() => {
     console.log("Forbundet til MongoDB");
 
-    console.log("🚀 NY VERSION DEPLOYET");
-
-    app.listen(PORT,'0.0.0.0', () => {
-        console.log(`Server kører på port ${PORT}`);
-    });
+   
 
 })
 .catch(err => {
