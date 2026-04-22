f2308c96995a86d1c72fb1593a026bf83a82062c
MatiEvaDan <matilde.danielsen@gmail.com>
2026-04-22 10:30:14 +0200

    c

--- Backend-Nodejs/app.js
+++ Backend-Nodejs/app.js
@@ -18,7 +18,7 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Routes
+// Routess
 app.get('/', (req, res) => {
   console.log("🔥 ROOT RAMT");
   res.status(200).send('API virker');
