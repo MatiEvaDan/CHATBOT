739ee381ee99b192e2bc3fabc0f846298f781b36
MatiEvaDan <matilde.danielsen@gmail.com>
2026-05-06 10:12:35 +0200

    test 2

--- Backend-Nodejs/Routes/BookRoutes.js
+++ Backend-Nodejs/Routes/BookRoutes.js
@@ -2,18 +2,15 @@ const express = require('express');
 const multer = require('multer');
 const router = express.Router();
 const bookController = require('../Controllers/BookController.js');
-const storage = multer.memoryStorage()
-const upload = multer({ storage })
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 router.post('/books', bookController.create);
 router.delete('/books/:id', bookController.deleteById);
 router.get('/books/:id', bookController.findById);
 router.get('/books', bookController.getAll);
-//router.post('/image', upload.single('image'),bookController.uploadImage);
-router.post('/image', (req, res) => {
-  console.log("POST IMAGE RAMT");
-  res.send("POST virker");
-});
+router.post('/image', upload.single('image'),bookController.uploadImage);
+
 
 //redeploy
 
